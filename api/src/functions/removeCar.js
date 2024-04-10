8cdfd6af75510f41ac76fff0215d1824b2dec2fe
AJ-222 <2538862@students.wits.ac.za>
2024-04-10 22:06:57 +0200

    Possible fix

--- api/src/functions/removeCar.js
+++ api/src/functions/removeCar.js
@@ -6,7 +6,7 @@ const unparsedPath = path.resolve(__dirname,"cars.json");
 
 app.http('removeCar', {
     methods: ['DELETE'],
-    authLevel: `cars/{carId}`,
+    authLevel: "function" ,
     handler: async (request, context) => {
         const jsonSet = await fs.readFile(unparsedPath, "utf8");
         
